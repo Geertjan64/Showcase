f7e594cfcb366e621e5226268847713b409532a5
Geertjan64 <geertjanverkuil@gmail.com>
2023-11-01 19:35:36 +0100

    Add project files.

---
+++ Showcase/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Showcase.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
