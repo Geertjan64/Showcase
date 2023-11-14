257001fb932c9b591e6b4c4b2e6a4cb2e660cfd8
Geertjan64 <geertjanverkuil@gmail.com>
2023-11-14 19:55:32 +0100

    Jquery error

--- Showcase/Models/MailRequest.cs
+++ Showcase/Models/MailRequest.cs
@@ -4,10 +4,11 @@ namespace Showcase.Models
 {
     public class MailRequest
     {
-        public string Token { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string ToEmail = "mailservertestdev@gmail.com";
         public string FromEmail { get; set; }
+        public string Mobile { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
 
---
+++ Showcase/wwwroot/js/check-form.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $("#contactForm").validate({
+        rules: {
+            FirstName: {
+                required: true
+            },
+            LastName: {
+                required: true
+            },
+            Subject: {
+                required: true
+            },
+            FromEmail: {
+                required: true,
+                email: true
+            },
+            Mobile: {
+                required: true,
+                digits: true
+            },
+            Body: {
+                required: true
+            }
+        },
+        messages: {
+            FirstName: "Voer uw voornaam in",
+            LastName: "Voer uw achternaam in",
+            Subject: "Voer het onderwerp in",
+            FromEmail: {
+                required: "Voer uw e-mailadres in",
+                email: "Voer een geldig e-mailadres in"
+            },
+            Mobile: {
+                required: "Voer uw mobiele nummer in",
+                digits: "Voer alleen cijfers in"
+            },
+            Body: "Voer uw bericht in"
+        },
+        onkeyup: function (element, event) {
+            // Live validatie tijdens het typen
+            this.element(element);
+        },
+        onfocusout: function (element) {
+            // Validatie bij focusverlies
+            this.element(element);
+        }
+    });
+});
