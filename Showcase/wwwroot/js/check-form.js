0927d63fa5dacb7fd5b460a3139159bfc13eec43
Geertjan64 <geertjanverkuil@gmail.com>
2023-11-15 19:47:42 +0100

    Nicee! Working contactform with validation. Also removed scripts from html page.

--- Showcase/wwwroot/js/check-form.js
+++ Showcase/wwwroot/js/check-form.js
@@ -36,10 +36,30 @@
             },
             Body: "Voer uw bericht in"
         },
+
+        errorElement: "span", // Gebruik een span voor foutberichten
+        errorClass: "text-danger", // Klasse voor foutberichten
+
+        highlight: function (element, errorClass, validClass) {
+            // Voeg rode rand toe aan het ongeldige element
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+
+        unhighlight: function (element, errorClass, validClass) {
+            // Verwijder rode rand van het geldige element
+            $(element).removeClass("is-invalid").addClass("is-valid");
+        },
+
+        errorPlacement: function (error, element) {
+            // Plaats foutbericht naast het element
+            error.insertAfter(element);
+        },
+
         onkeyup: function (element, event) {
             // Live validatie tijdens het typen
             this.element(element);
         },
+
         onfocusout: function (element) {
             // Validatie bij focusverlies
             this.element(element);
---
+++ Showcase/wwwroot/js/recaptcha.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    grecaptcha.ready(function () {
+        grecaptcha.execute('@GoogleCaptchaConfig.Value.SiteKey', { action: 'submit' }).then(function (token) {
+            // Add your logic to submit to your backend server here.
+            console.log(token);
+            document.getElementById("ContactFormModelToken").value = token;
+        });
+    });
+});
