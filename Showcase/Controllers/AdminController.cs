a2df8421f3277e20aa79f4e33a4ab7bf63cfbede
Desktop <geertjanverkuil@gmail.com>
2024-09-16 14:34:02 +0200

    Added createrole

--- Showcase/Controllers/AdminController.cs
+++ Showcase/Controllers/AdminController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Showcase.Models;
 
 namespace Showcase.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
@@ -10,6 +13,37 @@ namespace Showcase.Controllers
         public AdminController(RoleManager<IdentityRole> roleManager)
         {
             _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = _roleManager.CreateAsync(identityRole).Result;
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Profile", "Contact");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
         }
     }
 }
