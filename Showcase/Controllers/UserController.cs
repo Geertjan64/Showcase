77cd20f342af65f92c49467102a98f5b9c3a7ba3
Desktop <geertjanverkuil@gmail.com>
2024-09-17 14:04:01 +0200

    Werkend spellen ophalen voor alle gebruikers!

--- Showcase/Controllers/UserController.cs
+++ Showcase/Controllers/UserController.cs
@@ -1,23 +1,42 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Showcase.Areas.Identity.Data;
+using Showcase.Data;
+using Showcase.Models;
 using System.Threading.Tasks;
 
 [Authorize(Roles = "Admin")]
 public class UserController : Controller
 {
     private readonly UserManager<ShowcaseUser> _userManager;
+    private readonly GameDbContext _gameDbContext;
 
-    public UserController(UserManager<ShowcaseUser> userManager)
+    public UserController(UserManager<ShowcaseUser> userManager, GameDbContext gameDbContext)
     {
         _userManager = userManager;
+        _gameDbContext = gameDbContext;
     }
 
     // Methode om alle geregistreerde gebruikers op te halen
-    public IActionResult ListUsers()
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> ListUsers()
     {
-        var users = _userManager.Users;
-        return View(users);
+        var users = await _userManager.Users.ToListAsync();
+
+        var userWithGames = users.Select(user => new UserWithGamesViewModel
+        {
+            UserId = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+            PlayedGames = _gameDbContext.GameResults
+                .Where(gr => gr.Player1Id == user.Id || gr.Player2Id == user.Id)
+                .ToList()
+        }).ToList();
+
+        return View(userWithGames);
     }
+
+
 }
---
+++ Showcase/Models/UserWithGamesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Showcase.Models
+{
+    public class UserWithGamesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IList<GameResultRecord> PlayedGames { get; set; }
+    }
+}
