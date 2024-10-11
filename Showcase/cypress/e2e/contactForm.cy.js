5790d8620b28f4e1dc4355804af5aca2841f1dc4
Desktop <geertjanverkuil@gmail.com>
2024-10-11 13:44:52 +0200

    Added contactform test

---
+++ Showcase/cypress/e2e/contactForm.cy.js
@@ -0,0 +1,25 @@
+﻿describe('Contactform test', () => {
+    beforeEach(() => {
+        cy.visit('https://localhost:32772/');
+    });
+
+    it('Should fill out and submit the contact form', () => {
+        cy.get('input[name="FirstName"]').type('John');
+        cy.get('input[name="LastName"]').type('Doe');
+        cy.get('input[name="Subject"]').type('Vraag over product');
+        cy.get('input[name="FromEmail"]').type('johndoe@example.com');
+        cy.get('input[name="Mobile"]').type('+31612345678');
+        cy.get('textarea[name="Body"]').type('Hallo, ik wil graag contact met u! Kunt u mij bellen? :).');
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('.alert-success').should('be.visible');
+        cy.get('.alert-success').contains('Het bericht is succesvol verzonden!');
+    });
+
+    it('Should display validation errors when fields are empty', () => {
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span.text-danger').should('have.length', 6);
+    });
+});
