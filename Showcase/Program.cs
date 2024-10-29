c16313cbb1fe451bdf244333b15c00692d4f1e09
Desktop <geertjanverkuil@gmail.com>
2024-10-29 17:32:31 +0100

    Test!

--- Showcase/Program.cs
+++ Showcase/Program.cs
@@ -122,8 +122,8 @@ app.UseCookiePolicy();
 app.Use(async (context, next) =>
 {
     //context.Response.Headers.Add("Cache-Control", "no-store, no-cache, must-revalidate");
-    context.Response.Headers.Add("Pragma", "no-cache");
-    context.Response.Headers.Add("Expires", "0");
+    //context.Response.Headers.Add("Pragma", "no-cache");
+    //context.Response.Headers.Add("Expires", "0");
     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
     context.Response.Headers.Add("X-Frame-Options", "DENY");
